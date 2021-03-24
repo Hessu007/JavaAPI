11df3a22c4c8103f37702621194d5328c78d9434
hessu <turunen.heikki@gmail.com>
2021-03-24 15:26:47 +0200

    kk

--- src/apitest.java
+++ src/apitest.java
@@ -1,27 +1,41 @@
-import java.net.URLEncoder;
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
-import kong.unirest.Unirest;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+
+import org.json.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 
 public class apitest {
 
     public static void testi(){
-        HttpResponse<JsonNode> response = Unirest.get("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=26.44&lat=61.25&units=metric&lang=fi")
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=38.5&lat=-78.5"))
                 .header("x-rapidapi-key", "dcebd980camsh4b70e29885b542bp1c5f39jsn2dbda1d3cb1d")
                 .header("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com")
-                .asJson();
-        System.out.println(response.getStatus());
-        System.out.println( response.getHeaders().get("Content-Type"));
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        /*JSONObject obj = new JSONObject(response);
+        System.out.println(obj.getInt("timezone"));
+
+
+        /*Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonParser jp = new JsonParser();
         JsonElement je = jp.parse(response.getBody().toString());
         String prettyJsonString = gson.toJson(je);
-        System.out.println(prettyJsonString);
+        System.out.println(prettyJsonString);*/
 
     }
 }
