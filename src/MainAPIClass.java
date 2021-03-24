7298297c51a5920e59ebd1fcc999001f1b74bd59
Hessu <turunen.heikki@gmail.com>
2021-03-24 08:32:38 +0200

    APIWeather

---
+++ src/MainAPIClass.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainAPIClass {
+
+    private static void createWindow(){
+        JFrame mainframe = new JFrame("Sää API");
+        mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel tulosLabel = new JLabel("Tähän tulee sää tiedot", SwingConstants.CENTER);
+        tulosLabel.setPreferredSize(new Dimension(300,100));
+        mainframe.getContentPane().add(tulosLabel, BorderLayout.CENTER);
+
+        mainframe.setLocationRelativeTo(null);
+        mainframe.pack();
+        mainframe.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        createWindow();
+    }
+}
