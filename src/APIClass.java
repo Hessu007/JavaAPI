0b1fe76df100001db4136e9512aeb9c2bc2352d2
Hessu <turunen.heikki@gmail.com>
2021-03-25 09:19:50 +0200

    UpDate 25.3.2021

--- src/APIClass.java
+++ src/APIClass.java
@@ -3,9 +3,11 @@ import kong.unirest.Unirest;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.swing.*;
+
 public class APIClass {
 
-    public static void kuorttiWeather(){
+    public static void kuorttiWeather(JLabel tulosLabel){
 
        HttpResponse<String> response = Unirest.get("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=26.44&lat=61.42&units=metric&lang=fi")
                 .header("x-rapidapi-key", "dcebd980camsh4b70e29885b542bp1c5f39jsn2dbda1d3cb1d")
@@ -13,11 +15,11 @@ public class APIClass {
                 //.asJson();
                 .asString();
         System.out.println(response.getStatus());
-        JsonMeth(response);
+        JsonMeth(response,tulosLabel);
 
     }
 
-    public static void JsonMeth(HttpResponse<String> response){
+    public static void JsonMeth(HttpResponse<String> response, JLabel tulosLabel){
 
         String jsonBodyString = response.getBody();
 
@@ -27,6 +29,7 @@ public class APIClass {
         for (int i = 0; i < n; ++i) {
             final JSONObject resdata = geodata.getJSONObject(i);
             System.out.println(resdata.getString("city_name"));
+            tulosLabel.setText(resdata.getString("city_name"));
             System.out.println("Auriko nousee "+ resdata.getString("sunrise") + " ja laskee " + resdata.getString("sunset")+ " tänään " );
 
         }
--- src/MainAPIClass.java
+++ src/MainAPIClass.java
@@ -5,20 +5,30 @@ import javax.swing.*;
 
 public class MainAPIClass {
 
-    private static void createWindow(){
+    private static  void createWindow() {
         JFrame mainframe = new JFrame("Mökkisää API");
         mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel tulosLabel = new JLabel("Tähän tulee sää tiedot", SwingConstants.CENTER);
-        tulosLabel.setPreferredSize(new Dimension(300,100));
-        mainframe.getContentPane().add(tulosLabel, BorderLayout.CENTER);
+        Container contentPane = mainframe.getContentPane();
+        contentPane.setLayout(new GridLayout());
 
-        mainframe.setLocationRelativeTo(null);
+        frameGUI(mainframe);
         mainframe.pack();
         mainframe.setVisible(true);
     }
 
+    public static void frameGUI(JFrame mainframe){
+        JLabel tulosLabel = new JLabel("Tähän tulee sää tiedot", SwingConstants.CENTER);
+        tulosLabel.setPreferredSize(new Dimension(300,100));
+
+        APIClass.kuorttiWeather(tulosLabel);
+
+
+
+        mainframe.getContentPane().add(tulosLabel, BorderLayout.CENTER);
+    }
+
     public static void main(String[] args){
         createWindow();
-        APIClass.kuorttiWeather();
+        //APIClass.kuorttiWeather();
     }
 }
