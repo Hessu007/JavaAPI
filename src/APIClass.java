c7e0a9efbf479983542abb55740cd8814f15c240
Hessu <turunen.heikki@gmail.com>
2021-03-25 08:46:52 +0200

    UpDate 24.3.2021

--- src/APIClass.java
+++ src/APIClass.java
@@ -1,40 +1,40 @@
-import com.google.gson.*;
 import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
-import kong.unirest.json.JSONArray;
-import kong.unirest.json.JSONObject;
-import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class APIClass {
 
+
+
     public static void kuorttiWeather(){
 
-        HttpResponse <JsonNode> response = Unirest.get("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=26.44&lat=61.42&units=metric&lang=fi")
+       HttpResponse<String> response = Unirest.get("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=26.44&lat=61.42&units=metric&lang=fi")
                 .header("x-rapidapi-key", "dcebd980camsh4b70e29885b542bp1c5f39jsn2dbda1d3cb1d")
                 .header("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com")
-                .asJson();
-
-
-
-        System.out.println(response.getHeaders().get("Content-Type"));
+                //.asJson();
+                .asString();
 
 
         System.out.println(response.getStatus());
-        JsonNode myJsonString = response.getBody();
-        //System.out.println(myJsonString);
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(String.valueOf(myJsonString));
-        JsonObject jsonObject = element.getAsJsonObject();
-        String lampo = jsonObject.get("temp").getAsString();
-        System.out.println(lampo);
-
-
-
+        JsonMeth(response);
 
     }
-    public static void kuorttiWeather2(){
 
+    public static void JsonMeth(HttpResponse<String> response){
+
+        String jsonBodyString = response.getBody();
+
+        final JSONObject obj = new JSONObject(jsonBodyString);
+        final JSONArray geodata = obj.getJSONArray("data");
+        final int n = geodata.length();
+        for (int i = 0; i < n; ++i) {
+            final JSONObject resdata = geodata.getJSONObject(i);
+            System.out.println(resdata.getString("city_name"));
+
+
+        }
     }
     }
 
