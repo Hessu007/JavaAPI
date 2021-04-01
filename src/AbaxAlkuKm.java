bbd40f405b1d54f8e5af3290e2dd04f595979ce6
Hessu <turunen.heikki@gmail.com>
2021-04-01 16:46:25 +0300

    1.4.2021

--- src/AbaxAlkuKm.java
+++ src/AbaxAlkuKm.java
@@ -1,9 +1,7 @@
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kong.unirest.HttpResponse;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,27 +9,27 @@ import java.util.List;
 
 public class AbaxAlkuKm {
 
-    public static void HaeAbaxAlkuKm() {
+    public static String HaeAbaxAlkuKm() {
         /*String abaxString = "https://api.fm-track.com/objects/1bc82938-1ef9-11e7-bbbd-d7878bc7cff3/coordinates?&version=1&api_key=2tvzJblm0JDJRznMzNQm-ZrQWC8T7tz8&fromDatetime=2021-03-16T05:14:24.000Z&toDatetime=2021-03-16T08:15:04.000Z";
 
         HttpResponse<String> paluuREST = AbaXapi.HttpResponse(abaxString);
         int tulos = paluuREST.getStatus();
         System.out.println(tulos);*/
-        private String jsonString;
+        String jsonString = null;
         String json = "[{\"name\": \"Java\", \"description\": \"Java is a class-based, object-oriented programming language.\"},{\"name\": \"Python\", \"description\": \"Python is an interpreted, high-level and general-purpose programming language.\"}, {\"name\": \"JS\", \"description\": \"JS is a programming language that conforms to the ECMAScript specification.\"}]";
          ObjectMapper Mapper = new ObjectMapper();
         try {
             //List<User> langs = Mapper.readValue(json, new TypeReference<List<User>>(){});
             User[] langs = Mapper.readValue(json, User[].class);
             List<String> langList = new ArrayList(Arrays.asList(langs));
-            String jsonString = Mapper.writeValueAsString(langList);
+            jsonString = Mapper.writeValueAsString(langList);
 
             System.out.println(jsonString);
 
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        return
+        return jsonString;
     }
 
 }
